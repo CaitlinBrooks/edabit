a3c10357c1cf2cd285680d4bf7f570a2146de6d0
CaitlinCodes <caiit@me.com>
2018-11-15 13:50:12 -0700

    longest word

--- index.js
+++ index.js
@@ -49,3 +49,20 @@ function reverseString(str) {
 }
 reverseString("hello");
 
+function findLongestWord(str) {
+  return str.length;
+}
+findLongestWord("The quick brown fox jumped over the lazy dog");
+
+function findLongestWord(str) {
+  var strSplit = str.split(' ');
+  var longestWord = 0;
+  for (var i = 0; i < strSplit.length; i++) {
+    if (strSplit[i].length > longestWord) {
+      longestWord = strSplit[i].length;
+    }
+  }
+  return longestWord;
+}
+findLongestWord("The quick brown fox jumped over the lazy dog");
+
