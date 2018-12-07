fffc37a4854d28e2a3d19a5c502a5f234c65d164
CaitlinCodes <caiit@me.com>
2018-12-07 15:33:24 -0700

    challenge

--- index.js
+++ index.js
@@ -131,3 +131,12 @@ console.log(WordCount("hello world"));
 //   }
 
 //   console.log findLargestNum(5, 10)
+
+function stretchTruth(num,str,arr) {
+	var x = 0, y = 1;
+  for (var i in arr) {
+		if (arr[i] === num) x++;
+		if (arr[i] === str) y++;
+	}
+	return x+y;
+}
